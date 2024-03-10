eee7d777d461c2c746eea76152c88c61751358b3
Daniel Gooden <dan.gooden.dev@gmail.com>
2024-03-10 22:35:24 +1100

    working as required

--- assignments/assignment1/exercise_1_dgooden.sql
+++ assignments/assignment1/exercise_1_dgooden.sql
@@ -5,7 +5,7 @@ CREATE TABLE customers (
   address VARCHAR(200),
   city VARCHAR(50),
   state VARCHAR(30) CHECK (state IN ('NSW', 'VIC', 'QLD', 'ACT', 'TAS', 'NT', 'SA', 'WA')),
-  postcode VARCHAR(8),
+  postcode VARCHAR(8)
 );
 
 CREATE TABLE movies (
@@ -23,8 +23,8 @@ CREATE TABLE stock (
   media_type VARCHAR(20) CHECK (media_type IN ('DVD', 'Blu-Ray', 'Stream-Media')),
   cost_price REAL CHECK (cost_price > 0),
   retail_price REAL CHECK (retail_price > 0),
-  current_stock REAL CHECK (current_stock > 0),
-  PRIMARY KEY (movie_id, media_type)
+  current_stock REAL CHECK (current_stock >= 0),
+  PRIMARY KEY (movie_id, media_type),
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
     ON DELETE CASCADE
 );
